ea4879beebbad46599c15012e495aa0b0e33709c
zzy <zhang.zhongyuan11@gmail.com>
2014-11-27 12:52:30 +0800

    Revamp tasks with class-style; update some codes to follow PEP 8 style guide

--- fabistrano/deploy.py
+++ fabistrano/deploy.py
@@ -1,120 +1,148 @@
-from fabric.api import env, sudo, run, put, task
-from fabistrano.helpers import with_defaults
+from fabric.api import env, run
+from fabric.tasks import Task
+from fabistrano.helpers import set_defaults, sudo_run
 
 
-VERSION = "0.2"
+VERSION = "0.4"
 
 env.timeout = 6000
 
-def sudo_run(*args, **kwargs):
-    if env.use_sudo:
-        sudo(*args, **kwargs)
-    else:
-        run(*args, **kwargs)
 
-@task
-@with_defaults
-def restart():
-    """Restarts your application"""
-    try:
-        run("touch %(current_release)s/%(wsgi_path)s" % \
-                { 'current_release': env.current_release,
-                  'wsgi_path': env.wsgi_path })
-    except AttributeError:
+class BaseTask(Task):
+    def __init__(self, *args, **kwargs):
+        super(BaseTask, self).__init__(*args, **kwargs)
+        set_defaults()
+
+    def run(self):
+        raise NotImplementedError
+
+
+class RestartTask(Task):
+    name = 'restart'
+
+    def run(self):
+        """Restarts your application"""
         try:
-            sudo_run(env.restart_cmd)
+            run('touch %(current_release)s/%(wsgi_path)s' %
+                {'current_release': env.current_release,
+                 'wsgi_path': env.wsgi_path})
         except AttributeError:
-            pass
+            try:
+                sudo_run(env.restart_cmd)
+            except AttributeError:
+                pass
 
-@with_defaults
+
 def permissions():
     """Make the release group-writable"""
-    sudo_run("chown -R %(user)s:%(group)s %(domain_path)s" % 
-            { 'domain_path':env.domain_path,
+    sudo_run('chown -R %(user)s:%(group)s %(domain_path)s' %
+             {'domain_path': env.domain_path,
               'user': env.remote_owner,
-              'group': env.remote_group })
-    sudo_run("chmod -R g+w %(domain_path)s" % { 'domain_path':env.domain_path })
+              'group': env.remote_group})
+    sudo_run('chmod -R g+w %(domain_path)s' % {'domain_path': env.domain_path})
 
-@task
-@with_defaults
-def setup():
-    """Prepares one or more servers for deployment"""
-    sudo_run("mkdir -p %(domain_path)s/{releases,shared}" % { 'domain_path':env.domain_path })
-    sudo_run("mkdir -p %(shared_path)s/{system,log}" % { 'shared_path':env.shared_path })
-    permissions()
 
-@with_defaults
+class SetupTask(Task):
+    name = 'setup'
+
+    def run(self):
+        """Prepares one or more servers for deployment"""
+        sudo_run('mkdir -p %(domain_path)s/{releases,shared}' % {'domain_path': env.domain_path})
+        sudo_run('mkdir -p %(shared_path)s/{system,log}' % {'shared_path': env.shared_path})
+        permissions()
+
+
 def checkout():
     """Checkout code to the remote servers"""
     from time import time
-    env.current_release = "%(releases_path)s/%(time).0f" % { 'releases_path':env.releases_path, 'time':time() }
-    run("cd %(releases_path)s; git clone -b %(git_branch)s -q %(git_clone)s %(current_release)s" % \
-        { 'releases_path':env.releases_path,
-          'git_clone':env.git_clone,
-          'current_release':env.current_release,
-          'git_branch':env.git_branch })
+    env.current_release = '%(releases_path)s/%(time).0f' % {'releases_path': env.releases_path, 'time': time()}
+    run('cd %(releases_path)s; git clone -b %(git_branch)s -q %(git_clone)s %(current_release)s' %
+        {'releases_path': env.releases_path,
+         'git_clone': env.git_clone,
+         'current_release': env.current_release,
+         'git_branch': env.git_branch})
 
-@task
-def update():
-    """Copies your project and updates environment and symlink"""
-    update_code()
-    update_env()
-    symlink()
-    set_current()
-    permissions()
 
-@task
-def update_code():
-    """Copies your project to the remote servers"""
-    checkout()
-    permissions()
+class UpdateTask(Task):
+    name = 'update'
 
-@with_defaults
+    def run(self):
+        """Copies your project and updates environment and symlink"""
+        UpdateCodeTask().run()
+        update_env()
+        symlink()
+        set_current()
+        permissions()
+
+
+class UpdateCodeTask(Task):
+    name = 'update_code'
+
+    def run(self):
+        """Copies your project to the remote servers"""
+        checkout()
+        permissions()
+
+
 def symlink():
     """Updates the symlink to the most recently deployed version"""
-    run("ln -nfs %(shared_path)s/log %(current_release)s/log" % { 'shared_path':env.shared_path, 'current_release':env.current_release })
+    run('ln -nfs %(shared_path)s/log %(current_release)s/log' %
+        {'shared_path': env.shared_path, 'current_release': env.current_release})
 
-@with_defaults
+
 def set_current():
     """Sets the current directory to the new release"""
-    run("ln -nfs %(current_release)s %(current_path)s" % { 'current_release':env.current_release, 'current_path':env.current_path })
+    run('ln -nfs %(current_release)s %(current_path)s' %
+        {'current_release': env.current_release, 'current_path': env.current_path})
 
-@with_defaults
+
 def update_env():
     """Update servers environment on the remote servers"""
-    sudo_run("cd %(current_release)s; pip install -r requirements.txt" % { 'current_release':env.current_release })
+    sudo_run('cd %(current_release)s; pip install -r requirements.txt' %
+             {'current_release': env.current_release})
     permissions()
 
-@task 
-@with_defaults
-def cleanup():
-    """Clean up old releases"""
-    if len(env.releases) > 3:
-        directories = env.releases
-        directories.reverse()
-        del directories[:3]
-        env.directories = ' '.join([ "%(releases_path)s/%(release)s" % { 'releases_path':env.releases_path, 'release':release } for release in directories ])
-        run("rm -rf %(directories)s" % { 'directories':env.directories })
 
-@with_defaults
+class CleanUpTask(Task):
+    name = 'cleanup'
+
+    def run(self):
+        """Clean up old releases"""
+        if len(env.releases) > 3:
+            directories = env.releases
+            directories.reverse()
+            del directories[:3]
+            env.directories = ' '.join(['%(releases_path)s/%(release)s' %
+                                        {'releases_path': env.releases_path, 'release': release} for release in directories])
+            run('rm -rf %(directories)s' % {'directories': env.directories})
+
+
 def rollback_code():
     """Rolls back to the previously deployed version"""
     if len(env.releases) >= 2:
         env.current_release = env.releases[-1]
         env.previous_revision = env.releases[-2]
-        env.current_release = "%(releases_path)s/%(current_revision)s" % { 'releases_path':env.releases_path, 'current_revision':env.current_revision }
-        env.previous_release = "%(releases_path)s/%(previous_revision)s" % { 'releases_path':env.releases_path, 'previous_revision':env.previous_revision }
-        run("rm %(current_path)s; ln -s %(previous_release)s %(current_path)s && rm -rf %(current_release)s" % { 'current_release':env.current_release, 'previous_release':env.previous_release, 'current_path':env.current_path })
+        env.current_release = '%(releases_path)s/%(current_revision)s' %\
+                              {'releases_path': env.releases_path, 'current_revision': env.current_revision}
+        env.previous_release = '%(releases_path)s/%(previous_revision)s' %\
+                               {'releases_path': env.releases_path, 'previous_revision': env.previous_revision}
+        run('rm %(current_path)s; ln -s %(previous_release)s %(current_path)s && rm -rf %(current_release)s' %
+            {'current_release': env.current_release, 'previous_release': env.previous_release, 'current_path': env.current_path})
 
-@task
-def rollback():
-    """Rolls back to a previous version and restarts"""
-    rollback_code()
-    restart()
 
-@task(default=True)
-def deploy():
-    """Deploys your project. This calls both `update' and `restart'"""
-    update()
-    restart()
+class RollBackTask(Task):
+    name = 'rollback'
 
+    def run(self):
+        """Rolls back to a previous version and restarts"""
+        rollback_code()
+        RestartTask().run()
+
+
+class DeployTask(Task):
+    name = 'deploy'
+
+    def run(self, default=True):
+        """Deploys your project. This calls both `update' and `restart'"""
+        UpdateTask().run()
+        RestartTask().run()
--- fabistrano/helpers.py
+++ fabistrano/helpers.py
@@ -1,45 +1,62 @@
+from os import path
 import functools
-from fabric.api import run, env
+from fabric.api import run, env, sudo
 
 
-def dir_exists(dir):
-    # return run('[ -d %s ] && echo 1 || echo 0' % dir) == '1'
-    pass
+def dir_exists(dir_path):
+    # why this line was comment out?
+    return run('[ -d %s ] && echo 1 || echo 0' % dir_path) == '1'
+
+
+def set_defaults():
+    if '_fabi_defaults' in env:
+        return
+
+    domain_path = path.join(env.base_dir, env.app_name)
+    # domain_path must be set separately, since it's referred in other defaults
+    env.setdefault('domain_path', domain_path)
+    defaults = (
+        ('use_sudo', True),
+        ('git_branch', 'master'),
+        ('python_bin', 'python'),
+        ('remote_owner', 'www-data'),
+        ('remote_group', 'www-data'),
+        ('current_path', path.join(env.domain_path, 'current')),
+        ('releases_path', path.join(env.domain_path, 'releases')),
+        ('shared_path', path.join(env.domain_path, 'shared')),
+    )
+    for k, v in defaults:
+        env.setdefault(k, v)
+
+    if dir_exists(env.releases_path):
+        env.releases = sorted(run('ls -x %(releases_path)s' % {'releases_path': env.releases_path}).split())
+
+        if len(env.releases) >= 1:
+            env.current_revision = env.releases[-1]
+            env.current_release = '%(releases_path)s/%(current_revision)s' % \
+                                  {'releases_path': env.releases_path,
+                                   'current_revision': env.current_revision}
+        if len(env.releases) > 1:
+            env.previous_revision = env.releases[-2]
+            env.previous_release = '%(releases_path)s/%(previous_revision)s' % \
+                                   {'releases_path': env.releases_path,
+                                    'previous_revision': env.previous_revision}
+
+    env._fabi_defaults = True
+
 
 def with_defaults(func):
     """A decorator that sets all defaults for a task."""
 
     @functools.wraps(func)
     def decorated(*args, **kwargs):
-        env.setdefault('use_sudo', True)
-        env.setdefault('git_branch', 'master')
-        env.setdefault('python_bin', 'python')
-        env.setdefault('remote_owner', 'www-data')
-        env.setdefault('remote_group', 'www-data')
-
-
-        env.setdefault('domain_path', "%(base_dir)s/%(app_name)s" % \
-                                  { 'base_dir':env.base_dir,
-                                    'app_name':env.app_name })
-        env.setdefault('current_path', "%(domain_path)s/current" % \
-                                       { 'domain_path':env.domain_path })
-        env.setdefault('releases_path', "%(domain_path)s/releases" % \
-                                        { 'domain_path':env.domain_path })
-        env.setdefault('shared_path', "%(domain_path)s/shared" % \
-                                      { 'domain_path':env.domain_path })
-        if not env.has_key('releases'):
-            if dir_exists(env.releases_path):
-                env.releases = sorted(run('ls -x %(releases_path)s' % { 'releases_path':env.releases_path }).split())
-
-                if len(env.releases) >= 1:
-                    env.current_revision = env.releases[-1]
-                    env.current_release = "%(releases_path)s/%(current_revision)s" % \
-                                                { 'releases_path':env.releases_path, 
-                                                  'current_revision':env.current_revision }
-                if len(env.releases) > 1:
-                    env.previous_revision = env.releases[-2]
-                    env.previous_release = "%(releases_path)s/%(previous_revision)s" % \
-                                                { 'releases_path':env.releases_path, 
-                                                  'previous_revision':env.previous_revision }
+        set_defaults()
         return func(*args, **kwargs)
     return decorated
+
+
+def sudo_run(*args, **kwargs):
+    if env.use_sudo:
+        sudo(*args, **kwargs)
+    else:
+        run(*args, **kwargs)

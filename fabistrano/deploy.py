85cb921fdeffbb024cf07c21e5759aba40d7d564
zzy <zhang.zhongyuan11@gmail.com>
2015-02-02 16:05:48 +0800

    Replace all 'run' with 'sudo_run'

--- fabistrano/deploy.py
+++ fabistrano/deploy.py
@@ -1,4 +1,4 @@
-from fabric.api import env, run
+from fabric.api import env
 from fabric.tasks import Task
 from fabistrano.helpers import set_defaults, sudo_run
 from fabistrano.deploy_strategies import remote_clone, local_clone
@@ -37,7 +37,7 @@ class RestartTask(BaseTask):
                 touch_file_path = '%(current_release)s/%(wsgi_path)s' %\
                                   {'current_release': env.current_release,
                                    'wsgi_path': env.wsgi_path}
-            run('touch %s' % touch_file_path)
+            sudo_run('touch %s' % touch_file_path)
         except AttributeError:
             try:
                 sudo_run(env.restart_cmd)
@@ -101,14 +101,14 @@ update_code = UpdateCodeTask()
 
 def symlink():
     """Updates the symlink to the most recently deployed version"""
-    run('ln -nfs %(shared_path)s/log %(current_release)s/log' %
-        {'shared_path': env.shared_path, 'current_release': env.current_release})
+    sudo_run('ln -nfs %(shared_path)s/log %(current_release)s/log' %
+             {'shared_path': env.shared_path, 'current_release': env.current_release})
 
 
 def set_current():
     """Sets the current directory to the new release"""
-    run('ln -nfs %(current_release)s %(current_path)s' %
-        {'current_release': env.current_release, 'current_path': env.current_path})
+    sudo_run('ln -nfs %(current_release)s %(current_path)s' %
+             {'current_release': env.current_release, 'current_path': env.current_path})
 
 
 def update_env():
@@ -129,7 +129,7 @@ class CleanUpTask(BaseTask):
             del directories[:3]
             env.directories = ' '.join(['%(releases_path)s/%(release)s' %
                                         {'releases_path': env.releases_path, 'release': release} for release in directories])
-            run('rm -rf %(directories)s' % {'directories': env.directories})
+            sudo_run('rm -rf %(directories)s' % {'directories': env.directories})
 
 cleanup = CleanUpTask()
 
@@ -143,8 +143,8 @@ def rollback_code():
                               {'releases_path': env.releases_path, 'current_revision': env.current_revision}
         env.previous_release = '%(releases_path)s/%(previous_revision)s' %\
                                {'releases_path': env.releases_path, 'previous_revision': env.previous_revision}
-        run('rm %(current_path)s; ln -s %(previous_release)s %(current_path)s && rm -rf %(current_release)s' %
-            {'current_release': env.current_release, 'previous_release': env.previous_release, 'current_path': env.current_path})
+        sudo_run('rm %(current_path)s; ln -s %(previous_release)s %(current_path)s && rm -rf %(current_release)s' %
+                 {'current_release': env.current_release, 'previous_release': env.previous_release, 'current_path': env.current_path})
 
 
 class RollBackTask(BaseTask):
